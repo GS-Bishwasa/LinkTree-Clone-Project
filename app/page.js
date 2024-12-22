d1f53e4af5dc024c324e5366c668f9f83c2c55cc
Bishwasa <giribiswas415@gmail.com>
2024-12-22 21:31:37 +0530

    Done

--- app/page.js
+++ app/page.js
@@ -2,8 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-   
-   </>
+    <>
+      <section className="min-h-[100vh] bg-green-700 grid grid-cols-2">
+        <div className="flex items-center justify-center flex-col ml-[10vw]">
+          <p className="text-yellow-300 font-bold text-[40px]">Everything you are. In one, simple link in bio.</p>
+          <p className="text-yellow-300 text-xl ">Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+        </div>
+
+        <div className="flex items-center justify-center flex-col mr-[10vw]">
+          dcadvdv
+        </div>
+      </section>
+      <section className="min-h-[100vh] bg-red-700">
+        vfvdfbdg
+      </section>
+    </>
   );
 }
