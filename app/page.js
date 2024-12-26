8a2a94fe5a8df2091b8f87e3592ecef88a5040e7
Bishwasa <giribiswas415@gmail.com>
2024-12-26 10:46:29 +0530

    Done

--- app/page.js
+++ app/page.js
@@ -1,21 +1,39 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <section className="min-h-[100vh] bg-green-700 grid grid-cols-2">
-        <div className="flex items-center justify-center flex-col ml-[10vw]">
-          <p className="text-yellow-300 font-bold text-[40px]">Everything you are. In one, simple link in bio.</p>
-          <p className="text-yellow-300 text-xl ">Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
-        </div>
 
-        <div className="flex items-center justify-center flex-col mr-[10vw]">
-          dcadvdv
-        </div>
+  const router = useRouter()
+  const [text, setText] = useState("")
+
+  
+  const createTree = () => { 
+    
+    router.push(`/generate?handle=${text}`)
+  }
+  return (
+    <main>
+      <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
+    <div className="flex justify-center flex-col ml-[10vw] gap-3">
+      <p className="text-yellow-300 font-bold text-6xl">Everything you </p>
+      <p className="text-yellow-300 font-bold text-6xl">are. In one,</p>
+      <p className="text-yellow-300 font-bold text-6xl">simple link in bio.</p>
+      <p className="text-yellow-300 text-xl my-4">Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+      <div className="input flex gap-2">
+        <input value={text} onChange={(e)=> setText(e.target.value)} className="px-2 py-2 focus:outline-green-800 rounded-md" type="text" placeholder="Enter your Handle" />
+        <button onClick={()=> createTree()} className="bg-pink-300 rounded-full px-4 py-4 font-semibold">Claim your Bittree</button>
+      </div>
+    </div>
+    <div className="flex mt-20 items-center justify-center flex-col mr-[10vw]">
+      <img className="w-[480px]" src="/image.png" alt="homepage image" />
+
+    </div>
       </section>
-      <section className="min-h-[100vh] bg-red-700">
-        vfvdfbdg
+      <section className="bg-red-700 min-h-[100vh]">
+ 
       </section>
-    </>
+    </main>
   );
 }
